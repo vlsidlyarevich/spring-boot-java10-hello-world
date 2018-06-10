297d3d90e21c46482bca4ecaf69897a840347b9e
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2018-06-10 18:34:48 +0300

    Initial commit

---
+++ src/test/java/com/github/vlsidlyarevich/spring_boot2_java10_hello/SpringBoot2Java10HelloApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.vlsidlyarevich.spring_boot2_java10_hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot2Java10HelloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
